1e8951f0dfe582d00332e22083848ecdcec1743b
Homa Wong <homawong@gmail.com>
2016-03-29 14:16:47 -0700

    Add test.

--- test/test.ts
+++ test/test.ts
@@ -3,6 +3,7 @@ import test = require('blue-tape');
 import IScroll = require('iscroll');
 
 test('Iscroll', (t) => {
-  // const instance = new IScroll('abc');
+  const instance = new IScroll('body');
+  t.not(instance, null, 'new on body');
   t.end();
 });
